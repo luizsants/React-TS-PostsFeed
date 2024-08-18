17ea3a219b11cd9090d550b292ad95cc80148f7e
Luiz Fernando dos Santos <100363551+luizsants@users.noreply.github.com>
2024-08-18 15:44:13 +0000

    Commit made from CodeSandbox - LuizSaint
    Just acceptable

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { Post } from "./Post";
+import { Header } from "./components/Header";
 import "./styles.css";
 
 export function App() {
-    return (
-        <div className="">
-            <h1>Olá Luiz Fernando</h1>
-            <Post
-                author="Luiz Santos"
-                content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit debitis quidem nam quam id cupiditate dignissimos dolorum iste cum ab quas ad necessitatibus, repudiandae, adipisci ut atque quibusdam accusantium doloribus."
-            />
-        </div>
-    );
+  return (
+    <div className="">
+      <Header />
+
+      <Post author="Luiz Santos" content="Content" />
+    </div>
+  );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
-export function Header() {
-    return (
-        <strong>Ignite Feed</strong>
-    );
+import styles from "./Header.module.css";
 
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <strong>Ignite Feed</strong>;
+    </header>
+  );
 }
